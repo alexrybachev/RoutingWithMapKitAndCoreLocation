9081e8278fe3ea09140470b8bfdbbe5d2d51c00d
Alex Rybachev <rybachev.as@gmail.com>
2022-06-28 21:40:55 +0400

    Initial Commit

---
+++ RoutingWithMapKitAndCoreLocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RoutingWithMapKitAndCoreLocation
+//
+//  Created by Aleksandr Rybachev on 28.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
